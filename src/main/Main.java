cf58f93f1f6fe64cef676ccd03d1a98bca03d7ff
iljun <sumusb@naver.com>
2017-11-06 12:24:59 +0900

    git branch test

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,5 +3,9 @@ package main;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("코딩해라");
+		test();
+	}
+	static void test(){
+		System.out.println("git Test");
 	}
 }
